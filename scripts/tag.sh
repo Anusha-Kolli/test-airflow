cced5b5771bd9c488a8800965e5f2e2845fd8448
Anusha <kolli.anusha9@gmail.com>
2020-09-29 10:45:09 -0400

    try this one

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+export current_tag=$(git tag --list --merged HEAD --sort=-committerdate | grep -E '^v?[0-9]+.[0-9]+.[0-9]+$' | head -n1 | sed 's/^v//')
+export new_tag=$(cat CHANGELOG.md | awk  -v tag='"$current_tag"' '/Unreleased/ {p=1;next}; /'"$current_tag"'/ {p=0} p' | grep -E "^## " | sed 's/.*\[\([^]]*\)\].*/\1/g')
+
 current_commit=$(git rev-parse HEAD)
 
 remote=$(git config --get remote.origin.url)
 repo=$(basename $remote .git)
-body=$(cat CHANGELOG.md | sed -n '/'"$NEW_TAG"'/,/'"$CURRENT_TAG"'/ {/'"$NEW_TAG"'/!{/'"$CURRENT_TAG"'/!p;};}')
+export body=$(cat CHANGELOG.md | sed -n '/'"$new_tag"'/,/'"$current_tag"'/ {/'"$new_tag"'/!{/'"$current_tag"'/!p;};}')
 
-new_version="v$NEW_TAG"
+new_version="v$new_tag"
 
 curl -s -X POST https://api.github.com/repos/Anusha-Kolli/$repo/releases \
 -H "Authorization: token $GITHUB_TOKEN" \
