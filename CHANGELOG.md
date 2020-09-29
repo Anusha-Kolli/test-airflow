c42915ea0c04e449d0a49fc731a73a2c06a151fd
Anusha <kolli.anusha9@gmail.com>
2020-09-29 07:46:40 -0400

    update something

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Change Log 
+    All notable changes to this project will be documented in this file.
+    This project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+
+## [1.0.0]- 2020-09-29
+### Added:
+- new: nothing to update
+- new: appending new
+- fix: trying new
+### Fixed:
+- fix: test
+- fix: trying new
+### Updated:
+- new: nothing to update
+- feat: test update
+### Removed: 
+- remove: test
+- remove: removing lines from readme
+### Docs:
+- docs: updating doc
+
---
+++ scripts/tag.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+current_tag=$(git tag --list --merged HEAD --sort=-committerdate | grep -E '^v?[0-9]+.[0-9]+.[0-9]+$' | head -n1)
+
+current_commit=$(git rev-parse HEAD)
+
+new_tag="$(awk  -v tag='"$current_tag"' '/Unreleased/ {p=1;next}; /'"$current_tag"'/ {p=0} p' CHANGELOG.md | grep -E "^## " | awk -F '[\\[\\]]' '{print $2}')"
+
+remote=$(git config --get remote.origin.url)
+repo=$(basename $remote .git)
+body="$(sed -n '/'"$new_tag"'/,/'"$current_tag"'/ {/'"$new_tag"'/!{/'"$current_tag"'/!p;};}' CHANGELOG.md)"
+
+
+curl -s -X POST https://api.github.com/repos/Anusha-Kolli/$repo/releases \
+-H "Authorization: token e8c18400ea319ba5c090e117a6a464a4f0a39ca4" \
+-d @- << EOF
+{
+    "tag_name": "$new_tag",
+    "target_commitish": "master",
+    "name": "$new_tag",
+    "body": "$($body)",
+    "draft": false,
+    "prerelease": false
+}    
+EOF
+
