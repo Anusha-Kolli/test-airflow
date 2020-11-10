72593fa7a041f8cb8b18967a60ea06a8d71e9119
ANUSHA KOLLI <kolli.anusha9@gmail.com>
2020-11-10 16:06:36 -0500

    Update tag.sh

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,45 +1,32 @@
 #!/bin/bash
 
-export current_tag=$(git tag --list --merged HEAD --sort=-committerdate | grep -E '^v?[0-9]+.[0-9]+.[0-9]+$' | head -n1 | sed 's/^v//')
+set -e
 
-export new_tag=$(cat CHANGELOG.md | awk  -v tag='"$current_tag"' '/Unreleased/ {p=1;next}; /'"$current_tag"'/ {p=0} p' | grep -E "^## " | sed 's/.*\[\([^]]*\)\].*/\1/g')
+# Get the recent commit
+new_tag=$(git log --pretty=format:'%h' -n 1)
 
-function create_release() {
-    local current_commit remote repo body new_version
+function create_pre_release() {
+    local new_version
 
-    current_commit=$(git rev-parse HEAD)
-    remote=$(git config --get remote.origin.url)
-    repo=$(basename $remote .git)
-    export body=$(cat CHANGELOG.md | sed -n '/'"$new_tag"'/,/'"$current_tag"'/ {/'"$new_tag"'/!{/'"$current_tag"'/!p;};}' | awk '$1=$1' ORS='\\n' )
-    new_version="v$new_tag"
+    new_version="develop-$new_tag-beta"
 
-    echo "new_version"
-
-    curl -s -X POST https://api.github.com/repos/Anusha-Kolli/$repo/releases \
+    # API request to create a Release
+    curl -s -X POST $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases \
     -H "Authorization: token $GITHUB_TOKEN" \
     -d @- << EOF
     {
        "tag_name": "$new_version",
-       "target_commitish": "master",
+       "target_commitish": "develop",
        "name": "$new_version",
-       "body": "$body",
+       "body": "This is a pre-release",
        "draft": false,
        "prerelease": true
     }    
 EOF
 }
 
-function check_changelog() {
-    changelog_commit=$(git log v$current_tag..HEAD --oneline CHANGELOG.md)
-    if [[ -z "$changelog_commit" ]]; then
-    echo "CHANGELOG.md is not updated"
-    else
-    create_release
-    fi
-}
-
 function main() {
-  check_changelog
+  create_pre_release()
 }
 
 main
