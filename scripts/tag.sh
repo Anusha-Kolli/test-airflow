76f26de533b575506e50335b44f16945f70ec325
Anusha <kolli.anusha9@gmail.com>
2020-12-08 10:45:51 -0500

    updating script

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -4,6 +4,9 @@ export current_tag=$(git tag --list --merged HEAD --sort=-committerdate | grep -
 
 export new_tag=$(cat CHANGELOG.md | awk  -v tag='"$current_tag"' '/Unreleased/ {p=1;next}; /'"$current_tag"'/ {p=0} p' | grep -E "^## " | sed 's/.*\[\([^]]*\)\].*/\1/g')
 
+export imageName="test/testImage"
+export registry="anusha972/test" 
+
 function create_release() {
     local current_commit remote repo body new_version
 
@@ -29,14 +32,29 @@ function create_release() {
 EOF
 }
 
+function dockerImage_BuildandPush() {
+    docker build -t ${imageName}:${new_tag}
+    docker login ${registry} -u ${user} -p ${password}
+    docker tag ${imageName}:${new_tag} ${registry}/${imageName}:${new_tag}
+    docker push ${registry}/${imageName}:${new_tag}
+    # docker login ${dev_registry} -u ${user} -p ${password}
+    # docker tag ${imageName}:${new_tag} ${dev_registry}/${imageName}:${new_tag}
+    # docker push ${dev_registry}/${imageName}:${new_tag}
+
+}
+
+anusha972/test:tagname
+
 function check_changelog() {
     changelog_commit=$(git log v$current_tag..HEAD --oneline CHANGELOG.md)
     if [[ -z "$changelog_commit" ]]; then
     echo "CHANGELOG.md is not updated"
     else
     create_release
+    dockerImage_BuildandPush
     fi
 }
+
 
 function main() {
   check_changelog
